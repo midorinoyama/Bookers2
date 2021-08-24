3daa770332f4dc3906963e55ed390d13c31d03be
midorinoyama <fujiaqa@gmail.com>
2021-08-24 10:14:49 +0000

    [ADD]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,11 +12,16 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
 
-    book.save
-    redirect_to book_path(book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.page(params[:page])
+      render :index
+    end
   end
 
   def edit
@@ -24,9 +29,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+
   end
 
   def destroy

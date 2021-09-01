fe303f940c5af43d4718ca18fd62bc773d276792
midorinoyama <fujiaqa@gmail.com>
2021-09-01 08:40:52 +0000

    [ADD]いいね

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books
   resources :users, only: [:show, :edit, :update, :index]
-
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end

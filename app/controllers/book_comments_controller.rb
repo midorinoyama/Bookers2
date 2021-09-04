e1f32a33b3fee4b375bfb1c4852e1c0298539392
midorinoyama <fujiaqa@gmail.com>
2021-09-04 00:53:47 +0000

    [ADD]コメント機能部分テンプレート

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book.id)
+    else
+      render "books/show"
+    end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
     redirect_to request.referer
   end
 
